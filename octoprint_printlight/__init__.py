0899a7f5bcf0a630f3fdc4b93edbb97b798c9594
tom0521 <tsherwood99@gmail.com>
2021-01-08 02:24:00 +0000

    Created the base settings and navbar templates

--- octoprint_printlight/__init__.py
+++ octoprint_printlight/__init__.py
@@ -1,37 +1,22 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
 
-class PrintlightPlugin(octoprint.plugin.SettingsPlugin,
-                       octoprint.plugin.AssetPlugin,
+class PrintLightPlugin(octoprint.plugin.SettingsPlugin,
                        octoprint.plugin.TemplatePlugin):
 
 	##~~ SettingsPlugin mixin
 
 	def get_settings_defaults(self):
 		return dict(
-			# put your plugin's default settings here
-		)
+		        gpio=1
+	        )
 
-	##~~ AssetPlugin mixin
-
-	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
-		return dict(
-			js=["js/printlight.js"],
-			css=["css/printlight.css"],
-			less=["less/printlight.less"]
-		)
+        def get_template_vars(self):
+                return dict(
+                        gpio=self._settings.get_int(["gpio"])
+                )
 
 	##~~ Softwareupdate hook
 
@@ -64,13 +49,13 @@ __plugin_name__ = "Printlight Plugin"
 # Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
 # Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
 # compatibility flags according to what Python versions your plugin supports!
-#__plugin_pythoncompat__ = ">=2.7,<3" # only python 2
+__plugin_pythoncompat__ = ">=2.7,<3" # only python 2
 #__plugin_pythoncompat__ = ">=3,<4" # only python 3
 #__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
 
 def __plugin_load__():
 	global __plugin_implementation__
-	__plugin_implementation__ = PrintlightPlugin()
+        __plugin_implementation__ = PrintLightPlugin()
 
 	global __plugin_hooks__
 	__plugin_hooks__ = {
